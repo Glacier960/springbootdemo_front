a7ec8f2342d6794612aeb164d099d51e8a1acbf4
刘亮 <liang.liush@ele.me>
2019-09-21 12:11:56 +0800

    add init login and index file

---
+++ src/components/home/AppIndex.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="name">
+      Hello World!
+      <br><br>
+
+      刘安然欢迎你！！
+      
+    </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'AppIndex'
+  }
+</script>
+
+
+
+<style scoped>
+#name {
+  color:blueviolet;
+  border:1px solid rgb(220,200,210);
+  text-align:center;
+  line-height:45px;
+  box-sizing:border-box;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
 
 Vue.use(Router)
+//1.这里的app与index.html里的app没有任何关系，这里的app与下面css里的app有关系2.
+//export default 是 ES6 的语法，意思是将这个组件整体导出，之后就可以使用 import 导入组件了。大括号里的内容是这个组件的相关属性3.<router-view/>，
+//是一个容器，名字叫“路由视图”，意思是当前路由（ URL）指向的内容将显示在这个容器中。也就是说，其它的组件即使拥有自己的路由（URL，需要在 router 文件夹的 index.js 文件里定义），也只不过表面上是一个单独的页面，实际上只是在根组件 App.vue 中
 
 export default new Router({
   routes: [
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex
+    },
     {
       path: '/',
       name: 'HelloWorld',
